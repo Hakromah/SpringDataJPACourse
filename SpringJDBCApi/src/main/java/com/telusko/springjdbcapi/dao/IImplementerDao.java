65c01fbf384ca011efbd745fd8f7e4556211a84f
Hassan <kkromah12@gmail.com>
2024-07-09 02:15:46 +0300

    edited

--- SpringJDBCApi/src/main/java/com/telusko/springjdbcapi/dao/IImplementerDao.java
+++ SpringJDBCApi/src/main/java/com/telusko/springjdbcapi/dao/IImplementerDao.java
@@ -4,5 +4,5 @@ import java.util.List;
 
 public interface IImplementerDao {
 
-    List<Implementer> getImplementerInfo(); //a collection
+    List<Implementer> getImplementerInfo(); //collection
 }
